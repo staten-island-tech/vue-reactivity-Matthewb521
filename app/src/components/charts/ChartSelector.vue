02fbd49956050530245b7e62c45bf67ebba54fb5
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2025-02-15 01:03:09 -0500

    Update ChartSelector.vue

--- app/src/components/charts/ChartSelector.vue
+++ app/src/components/charts/ChartSelector.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex justify-center items-center h-screen relative">
     <select
       v-model="selectedComponent"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute top-10 left-5"
     >
       <option value="chart">Show Doughnut Chart</option>
       <option value="BarChart">Show Bar Chart</option>
     </select>
 
-    <DoughnutChart v-if="selectedComponent === 'chart'" />
-    <BarChart v-if="selectedComponent === 'BarChart'" />
+    <div class="flex justify-center items-center">
+      <DoughnutChart v-if="selectedComponent === 'chart'" />
+      <BarChart v-if="selectedComponent === 'BarChart'" />
+    </div>
   </div>
 </template>
 
